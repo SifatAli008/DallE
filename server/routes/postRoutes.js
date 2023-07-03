9579aae5c1ed2ec9807016523dc9e105a0d3b567
Sifat Ali <90409992+SifatAli008@users.noreply.github.com>
2023-07-03 17:27:25 +0600

    First commit

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,44 @@
+// postRoutes.js
+
+import express from 'express';
+import { v2 as cloudinary } from 'cloudinary';
+
+import Post from '../mongodb/models/post.js';
+
+const router = express.Router();
+
+cloudinary.config({
+  cloud_name: process.env.COUDINARY_CLOUD_NAME,
+  api_key: process.env.COUDINARY_API_KEY,
+  api_secret: process.env.COUDINARY_API_SECRET,
+});
+
+// Get all posts
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+// Create a new post
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
+
+    res.status(201).json({ success: true, data: newPost });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
+
+export default router;
